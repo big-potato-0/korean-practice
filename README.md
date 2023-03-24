511d2892926dfdfc282c349b6a55bb304ac92015
potato <dead.deku@gmail.com>
2023-03-23 21:34:47 -0700

    moved files

--- README.md
+++
@@ -1,3 +0,0 @@
-# korean-practice
-wanted to try to code exercises to help me practice learning very basic korean
-i have a very limited vocabulary.
--- src/pages/directory/index.js
+++ src/pages/directory/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import _ from 'lodash'
-import { places } from '../../korean/vocabulary/nouns'
 import { Card, CardContent, CardHeader, Divider, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import { places } from '../../korean/vocabulary/nouns'
 
 const useStyles = makeStyles((theme) => ({
   container: {
